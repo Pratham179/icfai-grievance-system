f8d9b9a4ef1c0a045edfbdfe234a91ec3c77df03
Pratham Agrawal <prathamagrawal179@gmail.com>
2025-12-05 15:23:43 +0530

    CORS updated

--- api/server.js
+++ api/server.js
@@ -18,7 +18,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: "https://icfaigrivance.netlify.app/", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
