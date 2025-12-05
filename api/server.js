15e3c7b9f6fc0d1568150b98a9222d6d3f214101
Pratham Agrawal <prathamagrawal179@gmail.com>
2025-12-05 16:01:36 +0530

    phase 1 completed

--- api/server.js
+++ api/server.js
@@ -18,7 +18,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: "https://icfaigrivance.netlify.app/", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
