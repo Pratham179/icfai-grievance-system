398032f9448750bb046add60e4c0e9508a0d8695
Pratham Agrawal <prathamagrawal179@gmail.com>
2025-12-05 15:17:25 +0530

    backend url added

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://icfai-grievance-system.onrender.com",
   withCredentials: true, // for cookies
 });
 
